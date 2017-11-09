e2d2f9d434261d804762f640b3021bd9562fea1f
leonardobsi <leonardobsi@gmail.com>
2017-11-09 16:00:50 -0200

    Add files via upload

---
+++ src/br/com/leeo/Updates.java
@@ -0,0 +1,28 @@
+package br.com.leeo;
+
+import java.util.List;
+
+import br.com.leeo.jtelegram.Update;
+
+public class Updates {
+
+	private boolean ok;
+	private List<Update> result;
+
+	public boolean isOk() {
+		return ok;
+	}
+
+	public void setOk(boolean ok) {
+		this.ok = ok;
+	}
+
+	public List<Update> getResult() {
+		return result;
+	}
+
+	public void setResult(List<Update> result) {
+		this.result = result;
+	}
+
+}
